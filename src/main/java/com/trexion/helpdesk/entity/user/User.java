4be5c0450ef9ed817f0ecaf8c06b06278e9e0c96
Santiago Maldini <santimaldini@gmail.com>
2021-08-17 23:31:40 -0300

    Added missing properties

--- src/main/java/com/trexion/helpdesk/entity/user/User.java
+++ src/main/java/com/trexion/helpdesk/entity/user/User.java
@@ -1,13 +1,16 @@
 package com.trexion.helpdesk.entity.user;
 
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.validation.constraints.Email;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
@@ -24,16 +27,32 @@ public class User {
     @EqualsAndHashCode.Include
     private UUID id;
     @NonNull
+    @Column(nullable = false)
     private String firstName;
     @NonNull
+    @Column(nullable = false)
     private String lastName;
     @NonNull
+    @Column(nullable = false,unique = true)
     private String userName;
     @NonNull
+    @Column(nullable = false,unique = true)
     @Email
     private String email;
     @NonNull
+    @Column(nullable = false)
     private Integer phone;
     @NonNull
     private String image;
+    @Builder.Default
+    @Column(nullable = false, columnDefinition = "boolean default true")
+    private boolean active = true;
+    @CreationTimestamp
+    @NonNull
+    @Column(nullable = false)
+    private LocalDateTime createDateTime;
+    @UpdateTimestamp
+    @NonNull
+    @Column(nullable = false)
+    private LocalDateTime updateDateTime;
 }
