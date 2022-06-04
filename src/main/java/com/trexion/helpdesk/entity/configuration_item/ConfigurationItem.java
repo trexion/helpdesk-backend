18bbee121fd76985993a9ae3c0a90091022a945a
Santiago Maldini <santimaldini@gmail.com>
2022-06-04 16:23:48 -0300

    Added Configuration Item

--- src/main/java/com/trexion/helpdesk/entity/configuration_item/ConfigurationItem.java
+++ src/main/java/com/trexion/helpdesk/entity/configuration_item/ConfigurationItem.java
@@ -1,8 +1,6 @@
-package com.trexion.helpdesk.entity.group;
+package com.trexion.helpdesk.entity.configuration_item;
 
-import com.trexion.helpdesk.entity.configuration_item.ConfigurationItem;
-import com.trexion.helpdesk.entity.role.AccessRole;
-import com.trexion.helpdesk.entity.role.RoleAdmin;
+import com.trexion.helpdesk.entity.group.Group;
 import com.trexion.helpdesk.entity.ticket.Ticket;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
@@ -17,21 +15,18 @@ import java.util.List;
 @Builder
 @AllArgsConstructor
 @Data
-@Table(name = "user_group")
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class Group {
+public class ConfigurationItem {
     @Id
+    @Column
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private Integer id;
     @NonNull
     @Column(nullable = false)
     private String name;
-    @NonNull
-    @Column(nullable = false)
-    private String description;
     @Builder.Default
-    @Column(columnDefinition = "boolean default true")
+    @Column(nullable = false, columnDefinition = "boolean default true")
     private boolean active = true;
     @CreationTimestamp
     @NonNull
@@ -41,12 +36,9 @@ public class Group {
     @NonNull
     @Column(nullable = false)
     private LocalDateTime updateDateTime;
-    @OneToMany(mappedBy = "group")
-    private List<GroupAdmin> groupAdmins;
-    @OneToMany(mappedBy = "group")
-    private List<AccessGroup> accessGroups;
-    @OneToMany(mappedBy = "group")
+    @OneToMany(mappedBy = "configurationItem")
     private List<Ticket> tickets;
-    @OneToMany(mappedBy = "group")
-    private List<ConfigurationItem> configurationItems;
+    @ManyToOne
+    @JoinColumn(name = "group_id")
+    private Group group;
 }
