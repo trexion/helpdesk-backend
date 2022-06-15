52df6c1d54c895b332abcbe919e986e556adc92d
Santiago Maldini <santimaldini@gmail.com>
2022-06-15 20:03:26 -0300

    Implemented Ticket status crud

--- src/main/java/com/trexion/helpdesk/service/ticket/TicketStatusService.java
+++ src/main/java/com/trexion/helpdesk/service/ticket/TicketStatusService.java
@@ -1,5 +1,6 @@
 package com.trexion.helpdesk.service.ticket;
 
+import com.trexion.helpdesk.dto.request.ticket.TicketStatusRequestDto;
 import com.trexion.helpdesk.dto.response.ticket.TicketStatusDto;
 import com.trexion.helpdesk.entity.ticket.TicketStatus;
 import com.trexion.helpdesk.repository.ticket.TicketStatusRepo;
@@ -17,15 +18,33 @@ public class TicketStatusService {
     private final TicketStatusRepo ticketStatusRepo;
 
     public List<TicketStatusDto> getAll() {
-        return ticketStatusRepo.findAll().stream().map(this::mapTicketStatusToDto).collect(Collectors.toList());
+        return ticketStatusRepo.findAll().stream().map(TicketStatusService::mapTicketStatusToDto).collect(Collectors.toList());
     }
 
-    public TicketStatusDto getStatus(Integer id) {
+    public TicketStatusDto getTicketStatus(Integer id) {
         return mapTicketStatusToDto(ticketStatusRepo.getById(id));
     }
 
+    public TicketStatusDto createTicketStatus(TicketStatusRequestDto ticketStatusRequestDto){
+        return mapTicketStatusToDto(ticketStatusRepo.save(TicketStatus.builder()
+                .name(ticketStatusRequestDto.getName())
+                .active(ticketStatusRequestDto.getActive())
+            .build()));
+    }
 
-    private TicketStatusDto mapTicketStatusToDto(TicketStatus ticketStatus) {
+    public TicketStatusDto editTicketStatus(TicketStatusDto ticketStatusDto){
+        TicketStatus ticketStatus = ticketStatusRepo.getById(ticketStatusDto.getId());
+        ticketStatus.setName(ticketStatusDto.getName());
+        return mapTicketStatusToDto(ticketStatusRepo.save(ticketStatus));
+    }
+
+    public void disableTicketStatus(Integer id){
+        TicketStatus ticketStatus = ticketStatusRepo.getById(id);
+        ticketStatus.setActive(false);
+        ticketStatusRepo.save(ticketStatus);
+    }
+
+    private static TicketStatusDto mapTicketStatusToDto(TicketStatus ticketStatus) {
         return TicketStatusDto.builder()
             .id(ticketStatus.getId())
             .name(ticketStatus.getName())
