cdbed2bf96b596bade069c25df37e2170bd9ce48
Santiago Maldini <santimaldini@gmail.com>
2022-06-15 21:36:52 -0300

    Implemented Ticket Priority CRUD

--- src/main/java/com/trexion/helpdesk/service/ticket/TicketPriorityService.java
+++ src/main/java/com/trexion/helpdesk/service/ticket/TicketPriorityService.java
@@ -1,5 +1,6 @@
 package com.trexion.helpdesk.service.ticket;
 
+import com.trexion.helpdesk.dto.request.ticket.TicketPriorityRequestDto;
 import com.trexion.helpdesk.dto.response.ticket.TicketPriorityDto;
 import com.trexion.helpdesk.entity.ticket.TicketPriority;
 import com.trexion.helpdesk.repository.ticket.TicketPriorityRepo;
@@ -20,8 +21,27 @@ public class TicketPriorityService {
         return ticketPriorityRepo.findAllByActiveIsTrue().stream().map(this::mapToTicketPriorityDto).collect(Collectors.toList());
     }
 
-    public TicketPriorityDto getPriority(Integer id) {
+    public TicketPriorityDto getTicketPriority(Integer id){
         return mapToTicketPriorityDto(ticketPriorityRepo.getById(id));
+    }
+
+    public TicketPriorityDto createTicketPriority(TicketPriorityRequestDto ticketPriorityRequestDto){
+        return mapToTicketPriorityDto(ticketPriorityRepo.save(TicketPriority.builder()
+                .name(ticketPriorityRequestDto.getName())
+                .active(ticketPriorityRequestDto.getActive())
+            .build()));
+    }
+
+    public TicketPriorityDto editTicketPriority(TicketPriorityDto ticketPriorityDto){
+        TicketPriority ticketPriority = ticketPriorityRepo.getById(ticketPriorityDto.getId());
+        ticketPriority.setName(ticketPriorityDto.getName());
+        return mapToTicketPriorityDto(ticketPriorityRepo.save(ticketPriority));
+    }
+
+    public void disableTicketPriority(Integer id){
+        TicketPriority ticketPriority = ticketPriorityRepo.getById(id);
+        ticketPriority.setActive(false);
+        ticketPriorityRepo.save(ticketPriority);
     }
 
     private TicketPriorityDto mapToTicketPriorityDto(TicketPriority ticketPriority) {
